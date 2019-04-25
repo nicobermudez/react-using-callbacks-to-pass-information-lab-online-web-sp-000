86b4c880856933f0790ce03d792d55d2f7c56d81
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-25 17:32:08 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -18,9 +18,7 @@ export default class Matrix extends Component {
     })
   }
 
-  getSelectedColor = () => {
-    (this.state.selectedColor)
-  }
+  getSelectedColor = () => (this.state.selectedColor)
 
   genRow = (vals) => (
     vals.map((val, idx) => <Cell key={idx} color={val} />)
